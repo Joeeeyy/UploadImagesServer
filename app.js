be016067de433780a8a6e1ef264a8f52cc74b504
joey_tings <joey@tingtingapp.com>
2017-12-21 08:05:18 +0530

    Node js Server to handle image uploads in jpeg format only

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const multer = require('multer');
+const fileType = require('file-type');
+const fs = require('fs');
+
+const app = express()
+const router = express.Router()
+
+const port = process.env.PORT || 9098
+
+const upload = multer({
+                        dest:'images/',
+                        limits: {fileSize:10485760, files:1},
+                        fileFilter: (req, file, callback) => {
+                          // if (!file.originalname.match(/\.(jpg|jpeg)$)/)) {
+                          if (!file.originalname.match(/\.(jpg|jpeg)$/)) {
+                              return callback(new Error('Only Upload .jpeg image files'), false)
+                          }
+                          callback(null, true);
+                        }
+                      }).single('image')
+
+router.post('/images/uploads', (req, res)=>{
+  upload(req, res, function(err) {
+    if (err) {
+      res.status(400).json({message:err.message})
+    } else {
+      let path = `/images/${req.file.filename}`
+      res.status(200).json({message: 'Upload Success', path: path})
+    }
+  })
+})
+
+router.get('/images/:imagename', (req, res)=>{
+  let imagename = req.params.imagename
+  let imagepath = __dirname + "/images/" + imagename
+  let image = fs.readFileSync(imagepath)
+  let mime = fileType(image).mime
+
+  res.writeHead(200, {'Content-Type': mime})
+  res.end(image, 'binary')
+
+})
+
+app.use('/', router)
+
+app.use((err, req, res, next)=>{
+  if (err.code === 'ENOENT') {
+    res.status(404).json({message: 'No Such Image Found'})
+  } else {
+    res.status(500).json({message: err.message})
+  }
+})
+
+app.listen(port)
+console.log(`Server runs at port ${port}`);
